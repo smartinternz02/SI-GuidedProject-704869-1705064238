52e8b3f94ad337c683b259724c541db142ae2e9e
Ritik <ritikbansal621@gmail.com>
2024-02-08 10:54:01 +0530

    Added new script to existing test case

--- Scripts/TC_CURA_FooterSection_003/TC_Cura_FooterSection_003_dribble_icon/Script1706696755681.groovy
+++ Scripts/TC_CURA_FooterSection_003/TC_Cura_FooterSection_003_dribble_icon/Script1706696755681.groovy
@@ -18,6 +18,7 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
+WebUI.maximizeWindow()
 
 WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
